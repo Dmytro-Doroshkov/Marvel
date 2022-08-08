fb0400ba53530f1487d107f730044ff4c1cf8978
Dmitriy <dmitriydoroshkov@gmail.com>
2022-08-09 00:20:58 +0300

    add MarvalService

---
+++ src/services/MarvelService.js
@@ -0,0 +1,20 @@
+
+
+class MarvalService {
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+
+    getAllCharacters = () => {
+        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?apikey=1acac6c1e7c3eeddbfccf0aa54eb70fc');
+    }
+}
+
+export default MarvalService;
