84cc32d4810ad35a8c868dc071320b664cde098b
Dmytro <dollardimon@gmail.com>
2022-08-23 01:17:41 +0300

    heroku

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,10 @@
 
 
-class MarvalService {
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public';
+    _apiKey = 'apikey=1acac6c1e7c3eeddbfccf0aa54eb70fc';
+    _baseOffset = 210;
+
     getResource = async (url) => {
         let res = await fetch(url);
 
@@ -12,9 +16,32 @@ class MarvalService {
     }
 
 
-    getAllCharacters = () => {
-        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?apikey=1acac6c1e7c3eeddbfccf0aa54eb70fc');
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}/characters?limit=9&offset=${offset}&${this._apiKey}`);
+        return res.data.results.map(item => this._transformCharacter(item));
+    }
+
+
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}/characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+
+
+    _transformCharacter = (char) => {
+
+        return {
+            id: char.id,
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : "Sorry..." ,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+            comics: char.comics.items
+        }
     }
 }
 
-export default MarvalService;
+export default MarvelService;
